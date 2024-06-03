103a76b76aa70911da5a187508aa2f47c0c6657d
Your Name <you@example.com>
2024-06-03 09:35:20 +0300

    AA

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Option from "./option";
+import React,{useState} from "react";
+import ToFirst from "./toFirst";
 
 function App() {
+  const [show,setShow]=useState(true);
+
+  const Option=()=>{
+    setShow(false);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+   <h1>hi  </h1>
+   { show && <ToFirst AAA={Option} txt="Hi I am efrat and I am genius"/>}
     </div>
   );
 }
